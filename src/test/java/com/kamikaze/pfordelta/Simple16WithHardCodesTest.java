39ab914d4dce776533a051b77dbbe61beb230758
YukiXueli <echolixue@gmail.com>
2016-12-09 10:21:01 -0500

    Add files via upload

--- src/test/java/com/kamikaze/pfordelta/Simple16WithHardCodesTest.java
+++ src/test/java/com/kamikaze/pfordelta/Simple16WithHardCodesTest.java
@@ -19,5 +19,26 @@ public class Simple16WithHardCodesTest {
         for (int k = 0; k < 1024; ++k) input[k] = k;
         int k = sm.s16Compress(output, 0, input, 0, 1024, 10, 10, input);
         assertEquals(k, 8);
+
+        assertEquals(sm.s16CompressBackup(output, 0, input, 0, 1024, 10, 10, input), 8);
+
+        assertEquals(sm.s16Decompress(output, 0, input, 0, 1024), 28);
+
+        assertEquals(sm.s16DecompressWithIntBufferBackup(output, 0, 22, 1024), 28);
+
+        assertEquals(sm.s16DecompressWithIntBuffer(output, 0, 22, 1024), 28);
+
+        assertEquals(sm.s16DecompressWithIntBufferWithHardCodes(output, 0, 22, 1024), 28);
+
+        assertEquals(sm.s16DecompressWithIntBufferIntegrated(output, 0, 22, 1024, input, 8), 28);
+
+        assertEquals(sm.s16DecompressWithIntBufferIntegrated2(output, 0, 22, 1024, input, 8), 28);
+
+        assertEquals(sm.s16DecompressWithIntBufferIntegratedBackup(output, 0, 22, 1024, input, 8), 28);
+
+        assertEquals(sm.s16DecompressOneNumberWithHardCodes(output, 0, 22, 0), 28);
+
+        assertEquals(sm.s16DecompressOneNumberWithHardCodesIntegrated(output, 0, 22, 0, 8, input), 28);
+
     }
 }
